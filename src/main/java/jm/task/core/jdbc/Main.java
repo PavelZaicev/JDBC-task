96046d31f5ade74cd48144707000db129effc03b
Pavel Zaitsev <zaichiiks@gmail.com>
2023-10-17 19:07:58 +0300

    Сделал задачу JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,9 +5,7 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
         UserService userService = new UserServiceImpl();
-        userService.dropUsersTable();
         userService.createUsersTable();
         System.out.println("Table created!");
 
